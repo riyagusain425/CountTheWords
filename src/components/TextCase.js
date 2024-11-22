b204b4089afba126f4c1040da1a56f2b2234601d
riyagusain425 <riyagusain425@gmail.com>
2024-11-22 23:08:13 +0530

    git commit -m "First commit donee"

---
+++ src/components/TextCase.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {useState} from'react';
+
+export default function TextCase(props) {
+    let[text,setText]=useState("");
+   const upperCase=()=>{
+        let newText=text.toUpperCase();
+     setText(newText);
+    }
+   const lowerCase=()=>{
+        let newText=text.toLowerCase();
+     setText(newText);
+    }
+    const handleOnChange=(event)=>{
+        setText(event.target.value)
+    }
+   
+  return (
+    <>
+        <div className="container">
+      <div className="form-group">
+    <label for="myBox"><h3>Enter your text to analyse here </h3></label>
+    <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange}rows="6"></textarea>
+  </div>
+  <button className="btn btn-primary mx-2" onClick={upperCase}>Convert to Uppercase</button>
+  <button className="btn btn-primary mx-2" onClick={lowerCase}>Convert to Lowercase</button>
+    </div>
+    <div className="container my-3">
+     <h4>Can be read in {0.08*text.length} minutes </h4>
+     <p>Total {text.split(' ').length-1} words , {text.length} characters</p>
+     <h4>Text Summary</h4>
+     <p>{text}</p>
+    </div>
+    </>
+  );
+}
