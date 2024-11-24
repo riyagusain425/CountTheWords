ebb950996c030f97d3b40241707e7208b76dbe9e
riyagusain425 <riyagusain425@gmail.com>
2024-11-24 19:51:08 +0530

    initial commit

--- src/components/TextCase.js
+++ src/components/TextCase.js
@@ -11,6 +11,10 @@ export default function TextCase(props) {
         let newText=text.toLowerCase();
      setText(newText);
     }
+    const clearText=()=>{
+      let newText="";
+      setText(newText);
+    }
     const handleOnChange=(event)=>{
         setText(event.target.value)
     }
@@ -19,15 +23,16 @@ export default function TextCase(props) {
     <>
         <div className="container">
       <div className="form-group">
-    <label for="myBox"><h3>Enter your text to analyse here </h3></label>
+    <label htmlFor="myBox"><h3>Enter your text to analyse here </h3></label>
     <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange}rows="6"></textarea>
   </div>
   <button className="btn btn-primary mx-2" onClick={upperCase}>Convert to Uppercase</button>
   <button className="btn btn-primary mx-2" onClick={lowerCase}>Convert to Lowercase</button>
+  <button className="btn btn-primary mx-2" onClick={clearText}>Clear</button>
     </div>
     <div className="container my-3">
      <h4>Can be read in {0.08*text.length} minutes </h4>
-     <p>Total {text.split(' ').length-1} words , {text.length} characters</p>
+     <p>Total {text.split(' ').length} words , {text.length} characters</p>
      <h4>Text Summary</h4>
      <p>{text}</p>
     </div>
